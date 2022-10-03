7acb92097deb8b1d9b5c2ace3196b93a000133b6
juugaya <51192393+juugaya@users.noreply.github.com>
2022-10-03 12:12:27 +0900

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 function db_connect(){ 
     try{$dsn ='mysql:dbname=todolist;host=localhost;charset=utf8';
 $user ='root'; 
-$password ='root1480';
+$password ='';//自分の環境のパスワードを
 $dbh = new PDO($dsn,$user,$password); 
 $dbh->query('SET NAMES utf8');
 $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
