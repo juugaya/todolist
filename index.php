2f293a52b567a3e0af99047121766074e1d40177
juugaya <51192393+juugaya@users.noreply.github.com>
2024-09-09 17:32:49 +0900

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if(isset($_POST['submit'])){
 	try{
 	$dsn ='mysql:dbname=todolist;host=localhost;charset=utf8';
 	$user ='root';
-	$password ='root1480';
+	$password ='kamkyo;
     $dbh= new PDO($dsn,$user,$password);
 	$dbh->query('SET NAMES utf8'); 
 	$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
